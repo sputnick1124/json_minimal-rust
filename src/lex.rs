28026936ed31e3f9f03852bc13aad3459ef7f04b
Nicklas Stockton <nicklas.stockton@ngc.com>
2020-10-15 09:32:04 -0400

    Add lexer to start simplifying code base

--- src/lex.rs
+++ src/lex.rs
@@ -1,10 +1,12 @@
+use std::iter;
+
 pub enum JsonToken {
     Char(JsonSyntax),
     String(String),
-    Num(String),
+    Number(f64),
     Bool(bool),
     Null,
-    LexError{ reason: String },
+    LexError { reason: String },
 }
 
 pub enum JsonSyntax {
@@ -15,110 +17,109 @@ pub enum JsonSyntax {
     Comma,
 }
 
-pub struct TokenStream<I> 
-    where I: Iterator,
-          I::Item: Into<char> + Copy,
-{
+pub struct TokenStream<I> {
     stream_in: I,
 }
 
-impl<I> Iterator for TokenStream<I> 
-    where I: Iterator,
-          I::Item: Into<char> + Copy,
+pub trait Tokenizable: Iterator {
+    fn tokenize(self) -> TokenStream<Self>
+    where
+        Self: Sized,
+        Self::Item: Into<char> + Copy,
+    {
+        TokenStream { stream_in: self }
+    }
+}
+
+impl<T: ?Sized> Tokenizable for T where T: Iterator {}
+
+impl<I> Iterator for TokenStream<I>
+where
+    I: Iterator,
+    I::Item: Into<char> + Copy,
 {
     type Item = JsonToken;
     fn next(&mut self) -> Option<Self::Item> {
-        while let Some(c) = self.stream_in.next() {
-            let c: char = c.into();
-            match c {
-                '"' => {
-                    let mut s = String::new();
-                    while let Some(c) = self.stream_in.next() {
-                        if c.into() == '"' {
-                            break
-                        } else {
-                            s.push(c.into());
-                        }
-                    }
-                    return Some(JsonToken::String(s))
-                }
+        if let Some(c) = self.stream_in.next() {
+            match c.into() {
+                '"' => Some(JsonToken::String(
+                    self.stream_in
+                        .by_ref()
+                        .take_while(|&c| c.into() != '"')
+                        .map(|c| c.into())
+                        .collect::<String>(),
+                )),
                 '-' | '0'..='9' => {
-                    let mut has_exponent = false;
-                    let mut n = String::new();
-                    n.push(c);
-                    while let Some(c) = self.stream_in.next() {
-                        let c: char = c.into();
-                        match c {
-                            '0'..='9' => n.push(c),
-                            'e' => {
-                                if !has_exponent {
-                                    if let Some(c) = self.stream_in.next() {
-                                        if c.into() == '-' {
-                                            if let Some(c_next) = self.stream_in.next() {
-                                                if '0' <= c_next.into() || c_next.into() <='9' {
-                                                    n.push('e');
-                                                    n.push(c.into());
-                                                    n.push(c_next.into());
-                                                    has_exponent = true;
-                                                } else {
-                                                    return Some(JsonToken::LexError{reason:"Improper scientific notation".to_owned()});
-                                                }
-                                            } else {
-                                                return Some(JsonToken::LexError{reason:"Stream ended before exponent was complete".to_owned()});
-                                            }
-                                        } else if '0' <= c.into() || c.into() <='9' {
-                                            n.push(c.into());
-                                            has_exponent = true;
-                                        }
-                                    }
-                                } else {
-                                    return Some(JsonToken::LexError{reason: "Found exponential markers in scientific notation".to_owned()});
-                                }
-                            }
-                            _ => {
-                                return Some(JsonToken::LexError{reason: "Not a valid number".to_owned()});
-                            }
-                        }
+                    let num_str = iter::once(c.into())
+                        .chain(
+                            self.stream_in
+                                .by_ref()
+                                .take_while(|&c| matches!(c.into(), '0'..='9' | '.' | 'e' | 'E'))
+                                .map(|c| c.into()),
+                        )
+                        .collect::<String>();
+                    if let Ok(num) = num_str.parse::<f64>() {
+                        Some(JsonToken::Number(num))
+                    } else {
+                        Some(JsonToken::LexError {
+                            reason: format!("Unable to lex {} as f64", num_str),
+                        })
                     }
-                    return Some(JsonToken::Num(n));
                 }
                 'n' => {
-                    let ull = self.stream_in.by_ref().take(3).map(|c| c.into()).collect::<String>();
-                    if ull == "ull".to_owned() {
-                        return Some(JsonToken::Null);
+                    let maybe_null = iter::once(c)
+                        .chain(self.stream_in.by_ref())
+                        .take(4)
+                        .map(|c| c.into())
+                        .collect::<String>();
+                    if maybe_null == "null" {
+                        Some(JsonToken::Null)
                     } else {
-                        return Some(JsonToken::LexError{reason: "Expected null value".to_owned()});
+                        Some(JsonToken::LexError {
+                            reason: "Expected null value".to_owned(),
+                        })
                     }
                 }
                 't' => {
-                    let rue = self.stream_in.by_ref().take(3).map(|c| c.into()).collect::<String>();
-                    if rue == "rue".to_owned() {
-                        return Some(JsonToken::Bool(true));
+                    let maybe_true = iter::once(c)
+                        .chain(self.stream_in.by_ref())
+                        .take(4)
+                        .map(|c| c.into())
+                        .collect::<String>();
+                    if maybe_true == "true".to_owned() {
+                        Some(JsonToken::Bool(true))
                     } else {
-                        return Some(JsonToken::LexError{reason: "Expected boolean true value".to_owned()});
+                        Some(JsonToken::LexError {
+                            reason: "Expected boolean true value".to_owned(),
+                        })
                     }
-
                 }
                 'f' => {
-                    let alse = self.stream_in.by_ref().take(4).map(|c| c.into()).collect::<String>();
-                    if alse == "alse".to_owned() {
-                        return Some(JsonToken::Bool(false));
+                    let maybe_false = iter::once(c)
+                        .chain(self.stream_in.by_ref())
+                        .take(5)
+                        .map(|c| c.into())
+                        .collect::<String>();
+                    if maybe_false == "false".to_owned() {
+                        Some(JsonToken::Bool(false))
                     } else {
-                        return Some(JsonToken::LexError{reason: "Expected boolean false value".to_owned()});
+                        Some(JsonToken::LexError {
+                            reason: "Expected boolean false value".to_owned(),
+                        })
                     }
-
                 }
-                '[' => {return Some(JsonToken::Char(JsonSyntax::LeftBracket));},
-                ']' => {return Some(JsonToken::Char(JsonSyntax::RightBracket));},
-                '{' => {return Some(JsonToken::Char(JsonSyntax::LeftBrace));},
-                '}' => {return Some(JsonToken::Char(JsonSyntax::RightBrace));},
-                ',' => {return Some(JsonToken::Char(JsonSyntax::Comma));},
-                ' ' | '\n' | '\t' | '\r' => {continue},
-                _ => {
-                    return Some(JsonToken::LexError{reason: format!("Found invalid character during lexing: {}", c)});
-                }
+                '[' => Some(JsonToken::Char(JsonSyntax::LeftBracket)),
+                ']' => Some(JsonToken::Char(JsonSyntax::RightBracket)),
+                '{' => Some(JsonToken::Char(JsonSyntax::LeftBrace)),
+                '}' => Some(JsonToken::Char(JsonSyntax::RightBrace)),
+                ',' => Some(JsonToken::Char(JsonSyntax::Comma)),
+                ' ' | '\n' | '\t' | '\r' => self.next(),
+                _ => Some(JsonToken::LexError {
+                    reason: format!("Found invalid character during lexing: {}", c.into()),
+                }),
             }
+        } else {
+            None
         }
-        None
     }
 }
